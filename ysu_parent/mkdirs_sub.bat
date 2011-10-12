3e13d0da2e966de86e229f5ba0661913c5e2f05d
hyt1900 <pc@ti.que.jp>
2011-10-13 02:34:55 +0900

    初期コミット

---
+++ ysu_parent/mkdirs_sub.bat
@@ -0,0 +1,12 @@
+cd %~dp0
+
+set OBJ_DIR=%1
+
+cd..\%OBJ_DIR%
+
+mkdir src\main\scala
+rem mkdir src\main\java
+mkdir src\main\resouces
+mkdir src\test\scala
+rem mkdir src\test\java
+mkdir src\test\resouces
---
+++ ysu_parent/mvn_any.bat
@@ -0,0 +1,11 @@
+cd %~dp0
+
+rem set MAVEN_HOME=C:\java\apache-maven-3.0.3
+rem set PATH=%PATH%;%MAVEN_HOME%\bin
+
+rem cd ..\ysu_io
+ 
+
+mvn deploy
+rem mvn install
+rem mvn package
